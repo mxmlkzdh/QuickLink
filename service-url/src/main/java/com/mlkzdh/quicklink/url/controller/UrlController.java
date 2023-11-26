9dcdecff2c8e8d695a43a53dc37f7da79e412f2b
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-11-26 08:21:58 -0500

    Import cleanup

--- service-url/src/main/java/com/mlkzdh/quicklink/url/controller/UrlController.java
+++ service-url/src/main/java/com/mlkzdh/quicklink/url/controller/UrlController.java
@@ -6,7 +6,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
