ee0bb6f3d659042816580f7a42251234775f02a8
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-11-27 17:43:12 -0500

    Add a timeout value of 3 seconds to the default HttpClientProvider

--- service-redirect/src/main/java/com/mlkzdh/quicklink/redirect/controller/network/HttpClientProvider.java
+++ service-redirect/src/main/java/com/mlkzdh/quicklink/redirect/controller/network/HttpClientProvider.java
@@ -2,6 +2,7 @@ package com.mlkzdh.quicklink.redirect.controller.network;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,7 +12,10 @@ public final class HttpClientProvider {
   @LoadBalanced
   @Bean
   public RestTemplate restTemplate() {
-    RestTemplate restTemplate = new RestTemplate();
+    HttpComponentsClientHttpRequestFactory clientHttpRequestFactory =
+        new HttpComponentsClientHttpRequestFactory();
+    clientHttpRequestFactory.setConnectTimeout(3000);
+    RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory);
     restTemplate.setErrorHandler(new RedirectResponseErrorHandler());
     return restTemplate;
   }
