945ba0324e2c7c8addbd6e543fb55698bb679449
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-12-01 19:59:46 -0500

    Add the RequestMapping annotation to AnalyticsController

--- service-analytics/src/main/java/com/mlkzdh/quicklink/analytics/controller/AnalyticsController.java
+++ service-analytics/src/main/java/com/mlkzdh/quicklink/analytics/controller/AnalyticsController.java
@@ -1,9 +1,11 @@
 package com.mlkzdh.quicklink.analytics.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.mlkzdh.quicklink.analytics.service.AnalyticsService;
 
+@RequestMapping("/api/v1")
 @RestController
 public class AnalyticsController {
 
