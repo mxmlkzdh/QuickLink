0283f2f408e877834ad2956379c5781a564d4be5
Max Malekzadeh <mxmlkzdh@gmail.com>
2023-12-02 08:35:24 -0500

    Add service-gateway

---
+++ service-gateway/src/test/java/com/mlkzdh/quicklink/gateway/GatewayServiceApplicationTest.java
@@ -0,0 +1,14 @@
+package com.mlkzdh.quicklink.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplication {
+
+  @Test
+  void contextLoads() {
+
+  }
+
+}
